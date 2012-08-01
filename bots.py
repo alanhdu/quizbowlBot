e7c679631907e96f02d87a06586f40a3572e34f2
Alan <alanhdu@gmail.com>
2012-08-01 13:56:09 -0400

    Created a working, although inaccurate, unigram bot. Fixed up the database some more, and resolved some issues regarding unicode encodings.

--- bots.py
+++ bots.py
@@ -9,6 +9,8 @@ from nltk.probability import *
 from django.db import models
 from engine.models import *
 
+import unicodedata
+from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize, sent_tokenize
 import os
 import urllib
@@ -85,68 +87,92 @@ class TrainedBot(Bot):
 
     def onStartQuestion(self):
         for label in self.features:
-            self.posterior[label] = 0
+            self.posterior[label] = log(1 / len(self.features))
         self.questionText = ""
 
-    def consider(self, word):
-        maxGain = 0
-        bestGuess = None
+    def consider(self):
+        bestGuess = None 
 
-        for label in posterior:
-            temp = 10 * exp(posterior[label]) - 5 * (1 - exp(posterior[label]))
-            if temp > maxGain:
+        for label in self.posterior:
+            if bestGuess == None:
                 bestGuess = label
-                maxGain = temp
+            elif self.posterior[label] > self.posterior[bestGuess]:
+                bestGuess = label
 
-        if maxGain > 0:
-            print bestGuess
+        normProb = exp(self.posterior[bestGuess])
+        normProb /= sum(exp(x) for x in self.posterior.itervalues())
+
+        temp = exp(self.posterior["Clarinet"])
+        temp /= sum(exp(x) for x in self.posterior.itervalues())
+
+        word = self.questionText.split()[-1]
+        #print bestGuess, exp(self.posterior[bestGuess]), exp(self.posterior["Clarinet"]), self.features[bestGuess].prob(word), self.features["Clarinet"].prob(word)
+        return bestGuess
 
     def onNewWord(self, word):
         # P(A|B) = P(B|A) * P(A) / P(B)
         # A is the label, B is the text given so far
         # Assuming every label is equally likely, on each word
         # you just need to update P(B|A) and P(B)
-        # P(B and word) = P(B) * P(word)
+        # P(text and word) = P(text) * P(word)
         # P(B and word | A) = P(B|A) * P(word|A)
+
+        word = re.sub(r"[^\w -]", "", word).lower()
+        self.questionText += " " + word
+        #print word,
 
         for label in self.posterior:
             self.posterior[label] += log(self.features[label].prob(word))
-            self.posterior[label] -= log(self.model.prob(word, []))
+            self.posterior[label] += 10          #Prevents probalities from going to 0
 
-        self.consider()
+        return self.consider()
+
 
     def train(self):
+        # Maybe some stemming later?
         docCount = FreqDist()
+        self.model = FreqDist()
+
         for text in self.documents.itervalues():
-            docCount.update(reduce(set.union,
-                            (set(word_tokenize(re.sub(r"[^\w -]", "", sent)))
-                             for sent in sent_tokenize(text))))
-
+            words = reduce(list.__add__, 
+                          (word_tokenize(re.sub(r"[^\w '-]", "", sent))
+                          for sent in sent_tokenize(text)))
+            docCount.update(set(words))
         print "Got doc count"
-
 
         for doc in self.documents:
             wordCount = FreqDist()
             for sent in sent_tokenize(self.documents[doc]):
-                sent = re.sub(r"[^\w -]", "", sent)
-                wordCount.update(word_tokenize(sent))
+                sent = re.sub(r"[^\w '-]", "", sent)
+                wordCount.update(word_tokenize(sent)) 
 
             for word in wordCount:
-                tfidf = log(len(self.documents) / docCount[word])
-                tfidf *= wordCount[word]
-                if tfidf < 3:
-                    del wordCount[word]
+                if docCount[word] / len(self.documents) > 0.5:
+                    wordCount[word] = 0
+                    wordCount.pop(word)
+                else:
+                    tfidf = log(len(self.documents) / docCount[word])
+                    tfidf *= wordCount[word]
+                    if tfidf < 3:
+                        wordCount[word] = 0
+                        wordCount.pop(word)
 
-            self.features[doc] = SimpleGoodTuringProbDist(wordCount)
-            print self.features[doc]
-    def constructModel(self):
-        counter = FreqDist()
+            self.model += wordCount
+            self.features[doc] = wordCount
+
+        for label in self.features:
+            t = self.features[label]
+            self.features[label] = WittenBellProbDist(t, self.model.B())
+        print "Trained Wikipedia"
+
         for question in Question.objects.filter(id__lt=1000):
             for sent in sent_tokenize(question.body):
-                sent = re.sub(r"[^\w -]", "", sent).lower()
-                counter.update(word_tokenize(sent))
+                sent = re.sub(r"[^\w '-]", "", sent).lower()
+                self.model.update(word_tokenize(sent))
+        print "Got Questions"
 
-        self.model = SimpleGoodTuringProbDist(counter)
+        self.model = WittenBellProbDist(self.model, self.model.B() + 1)
+        print "Finised Model"
 
     def loadDocuments(self):
         for document in os.listdir("wiki"):
@@ -158,14 +184,12 @@ class TrainedBot(Bot):
         for i, label in enumerate(Label.objects.all()):
             title = "_".join( (s[0].upper() + s[1:].lower()
                                for s in label.body.split()))
-
-            if os.path.exists("wiki/{0}".format(title)):
-                continue
-
             title, text = self.getWikipedia(title)
-            title.replace(" ", "_")
+            title = unicode(title.replace(" ", "_"), "utf-8")
 
-            if len(text) > 0:
+            if len(text) > 0 and len(title) > 0:
+                title = unicodedata.normalize("NFKD", title)
+                title = title.encode('ascii', 'ignore')
                 try:
                     fout = open("wiki/{0}".format(title), "w")
                 except UnicodeEncodeError:
@@ -174,20 +198,32 @@ class TrainedBot(Bot):
                 fout.write(text.lower())
                 fout.close()
 
-            if i > 100:
+                label.body = title
+                label.save()
+
+            if i > 120:
                 break
 
     def getWikipedia(self, title):
-        url = u""
         try:
-            url = u"http://en.wikipedia.org/w/index.php?action=raw&title={0}".format(title)
-        except UnicodeDecodeError:
             title = title.decode('utf-8')
-            url = u"http://en.wikipedia.org/w/index.php?action=raw&title={0}".format(title)
+        except UnicodeEncodeError:
+            title = title.encode('utf-8')
+            title = title.decode('utf-8')
+        url = u"http://en.wikipedia.org/w/index.php?action=raw&title={0}".format(title)
+
+        title = title.encode('utf-8')
+        if os.path.exists("wiki/{0}".format(title)):
+            return ("", "")
+
+        if title.startswith("List_of_"):
+            return ("", "")
+
 
         text = urllib.urlopen(url.encode("utf-8")).read()
 
         if len(text) == 0:
+            title = title.decode('utf-8')
             url = u"http://en.wikipedia.org/w/api.php?action=opensearch&search={0}".format(title)
             text = urllib.urlopen(url.encode("utf-8")).read()
 
@@ -214,19 +250,22 @@ class TrainedBot(Bot):
                     print title
                     return (title, "")
 
-        if "#REDIRECT" in text:
+        if "#REDIRECT" in text.upper():
             newTitle = text.split("]]")[0]
             newTitle = " ".join(newTitle.split()[1:]).translate(None, "[]")
 
             return self.getWikipedia(newTitle)
 
-        elif "{{disambig" in text or "{{surname" in text:
-            return (title, "")
+        else:
+            l = ["{{disambig", "surname", "hndis", "geodis"]
+            for search in l:
+                if search in text:
+                    return (title, "")
 
-        text = re.sub(r"==( )?See also( )?==(.|\n)*$", "", text)
-        text = re.sub(r"==( )?Notes( )?==(.|\n)*$", "", text)
-        text = re.sub(r"==( )?External Links( )?==(.|\n)*$", "", text)
-        text = re.sub(r"==( )?References( )?==(.|\n)*$", "", text)
+        text = re.sub(r"==( )?[Ss]ee [Aa]lso( )?==(.|\n)*$", "", text)
+        text = re.sub(r"==( )?[Nn]otes( )?==(.|\n)*$", "", text)
+        text = re.sub(r"==( )?[Ee]xternal [Ll]inks( )?==(.|\n)*$", "", text)
+        text = re.sub(r"==( )?[Rr]eferences( )?==(.|\n)*$", "", text)
         text = re.sub(r"==.*==", "", text)
 
         text = re.sub(r"<!(.|\n)*?>", "", text) 
@@ -240,42 +279,72 @@ class TrainedBot(Bot):
         text = re.sub(r"\[\[File(.|\n)*?]]", "", text)
 
         text = re.sub(r"{{.*?}}", "", text)
-        text = re.sub(r"{{(.|\n)*?}}", "", text)
+        text = re.sub(r"{\|(.|\n)*?}", "", text)
 
         text = re.sub(r"\[\[[^\]]*?\|", "", text)
+        text = re.sub(r"(\n)+", "\n", text)
 
         text = text.replace("&nbsp;", " ")
         text = text.translate(None, "'[]")
 
         return (title, text.strip())
 
-    def consider(self):
-        newWord = self.questionText.split(" ")[-1]
 
-        # Needs some form of smoothing
-        for label in posterior:
-            if newWord in self.features[label][newWord]:
-                posterior[label] += self.features[label][newWord]
-
-
+def getBot():
+    bot = TrainedBot(None)
+    bot.loadDocuments()
+    bot.train()
+    return bot
 
 if __name__ == "__main__":
     bot = TrainedBot(None)
-    bot.downloadDocuments()
-    bot.constructModel()
+    #bot.downloadDocuments()
     bot.loadDocuments()
-    print "Loaded documents"
     bot.train()
+
+
+    fout = open('a.txt', "w")
+    for key in bot.features:
+        ls = Label.objects.filter(body=key)
+        #print key
+        label = ls[0]
+        if len(ls) > 1:
+            qids = []
+            for l in ls[1:]:
+                qids += [q.id for q in l.questions.all()]
+            for l in ls[1:]:
+                l.delete()
+
+            for qid in qids:
+                label.questions.add(Question.objects.get(id=qid))
+
+        #label = Label.objects.get(body="Clarinet")
+        print >>fout, label.body
+        for question in label.questions.all():
+            print >>fout, "Question:", len(question.body.split())
+            bot.onStartQuestion()
+
+            prevTime = -2
+
+            for i, word in enumerate(question.body.split()):
+                answer = bot.onNewWord(word)
+
+                if answer == label.body:
+                    #if i > prevTime + 1:
+                    #    print >>fout
+                    prevTime = i
+                    print >>fout, i,
+
+            print >>fout 
+        print >>fout
+
+    fout.close()
+
+    """
+    l = Label.objects.get(body__startswith=u"Piet\xe0_")
+    q = l.questions.get()
+
     bot.onStartQuestion()
-    bot.onNewWord("A")
-    #print bot.getWikipedia("James_Sidney_Ensor")
-
-"""
-from BridgePython import Bridge
-bridge = Bridge(api_key="60707403e0bf87e4")
-match = bridge.get_service("match_22")
-bot = RepeatBot(match)
-match.addPlayer(bot)
-
-bridge.connect()
-"""
+    for word in q.body.split():
+        bot.onNewWord(word)
+    """
--- move.py
+++ move.py
@@ -1,4 +1,28 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizBowl.settings")
+
+import re
 import sqlite3
+
+def stringDistance(a, b):
+    a = " " + a
+    b = " " + b
+    distance = [ [0 for x in b] for y in a]
+
+    for i in xrange(len(a)):
+        distance[i][0] = i
+    for j in xrange(len(b)):
+        distance[0][j] = j
+
+    for j in xrange(1, len(b)):
+        for i in xrange(1, len(a)):
+            if a[i] == b[j]:
+                distance[i][j] = distance[i - 1][j - 1]
+            else:
+                distance[i][j] = min(distance[i - 1][j] + 1,
+                                     distance[i][j - 1] + 1,
+                                     distance[i - 1][j - 1] + 1)
+    return distance[-1][-1]
 
 def loadQuestions(fout, c):
     l = []
@@ -20,7 +44,7 @@ def loadLabels(fout1, fout2, c):
     for (aid, text) in c.execute("select answer_id, answer_text from cannonical_answer"):
         try:
             l1.append("|".join([str(aid), str(text)]))
-        except UnicodeEncodeError:
+        except UnicodeEncodeErrgeor:
             pass
 
         if aid > i:
@@ -54,11 +78,95 @@ def loadLabels(fout1, fout2, c):
     fout1.write("\n".join(l1))
     fout2.write("\n".join(l2))
 
-fout1 = open("labels.csv", "w")
-fout2 = open("mapping.csv", "w")
-conn1 = sqlite3.connect("questions.db")
-c1 = conn1.cursor()
+from engine.models import *
+def cleanupLabels():
+    i = 0
+    labels = Label.objects.all().order_by("body")
 
-loadLabels(fout1, fout2, c1)
-fout1.close()
-fout2.close()
+    while i != labels.count():
+        label = labels[i]
+        
+        if any(word[0].lower() == word[0] and (len(word) >= 3 or len(word) == 1)
+                and word != "the"
+                and word[0] not in "0123456789\""
+               for word in label.body.split()):
+            newBody = ""
+            words = label.body.split()
+
+            for (j, word) in enumerate(words):
+                if j == 0:
+                    newBody += word[0].upper() + word[1:] + " "
+                else:
+                    if "." in word or (len(word) < 3 and len(word) > 1) or word == "the":
+                        newBody += word + " "
+                    else:
+                        newBody += word[0].upper() + word[1:] + " "
+
+            newBody = newBody.strip()
+            
+            print "Capitalize", label.body, newBody
+            label.body = newBody
+            label.save()
+        if "<" in label.body:
+            label.body = re.sub("<.*>", "", label.body)
+            label.save()
+            print "Remove < >"
+        if "{" in label.body:
+            label.body = re.sub("{.*}", "", label.body)
+            label.save()
+            print "Remove { }"
+        if label.body.strip() != label.body:
+            print "strip", label.body,
+            label.body = label.body.strip()
+            label.save()
+            print label.body
+
+        temps = [Label.objects.filter(body=l.body)
+                 for l in labels
+                 if l.body.lower()[:5] == label.body.lower()[:5]
+                 and stringDistance(l.body.lower(), label.body.lower()) < len(l.body) / 10 + 1
+                 and "I" not in l.body
+                 ]
+        ls = []
+        for temp in temps:
+            for l in temp:
+                ls.append(l)
+        if len(ls) > 1:
+            for l in ls:
+                if "-" in l.body:
+                    label = l
+                if "'" in l.body:
+                    label = l
+                if "." in l.body:
+                    label = l
+
+            qids = []
+            for l in ls:
+                for q in label.questions.all():
+                    qids.append(q.id)
+
+            qids = list(set(qids))
+            print label.body, qids, [l.body for l in ls], len(ls) - 1
+
+            for l in ls:
+                if l.id != label.id:
+                    l.delete()
+
+            for qid in qids:
+                label.questions.add(Question.objects.get(id=qid))
+
+            label.body = label.body.strip()
+            label.save()
+
+            labels = Label.objects.all()
+        if len(label.questions.all()) == 0:
+            print "No answer", label.body
+            label.delete()
+            labels = Label.objects.all()
+
+        i += 1
+        if i % 100 == 0:
+            print i
+
+if __name__ == "__main__":
+    cleanupLabels()
